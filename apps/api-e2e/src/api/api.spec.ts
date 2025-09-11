94a3c826258e890abd346bdfa24a69c33ea42b83
Christian Felipe <chrisfelipe525@gmail.com>
2025-09-12 02:19:12 +0800

    fix: typecheck issue during CI workflow is resolved

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 describe('GET /api', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+    const baseURL = process.env['API_BASE_URL'] || 'http://localhost:3001';
+    const res = await axios.get(`${baseURL}/api`);
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
