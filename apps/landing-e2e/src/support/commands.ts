2847bb73d82499cecb123fb5ebcfc8d60915d8d5
Christian Felipe <chrisfelipe525@gmail.com>
2025-09-15 06:06:05 +0800

    chore: setup landing app

---
+++ apps/landing-e2e/src/support/commands.ts
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      login(email: string, password: string): void;
+    }
+  }
+}
+
+// -- This is a parent command --
+Cypress.Commands.add('login', (_email, _password) => {
+  // Custom command example: Login implementation would go here
+});
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
