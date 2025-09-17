82fd4057b3f23df6e86162af5e49794608e884d2
Christian Felipe <chrisfelipe525@gmail.com>
2025-09-18 04:00:16 +0800

    chore: configure web and api to have @hanapp as project name

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -2,11 +2,11 @@ import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  dir: __dirname,
+  dir: './',
 });
 
 const config: Config = {
-  displayName: 'web',
+  displayName: '@hanapp-ph/web',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
