a86d8852c2203c469858f02d935e90c388c2dcf2
Aenubrix <anubrix326@gmail.com>
2025-09-20 01:11:05 +0800

    feat: add login page frontend

--- apps/web/components/dialog-header.tsx
+++ apps/web/components/dialog-header.tsx
@@ -1,16 +1,42 @@
 'use client';
 
+import Image from 'next/image';
+
 interface DialogHeaderProps {
   className?: string;
-  imageClassName?: string;
+  // Removed imageClassName as width/height are now explicit
 }
 
 export function DialogHeader({ className = '' }: DialogHeaderProps) {
+  const mobileLogoWidth = 160;
+  const mobileLogoHeight = 50;
+  const smMobileLogoWidth = 250;
+  const desktopLogoWidth = 80;
+  const desktopLogoHeight = 80;
+
   return (
     <div
-      className={`text-center mb-4 sm:mb-16 mt-0 sm:mt-12 ml-4 sm:ml-0 ${className}`}
+      className={`text-center mb-4 sm:mb-16 mt-0 sm:mt-24 ml-4 sm:ml-0 ${className}`}
     >
-      <div className="mb-2 sm:mb-12 ml-4 sm:ml-0 flex justify-center"></div>
+      <div className="mb-2 sm:mb-12 ml-4 sm:ml-0 flex justify-center">
+        <Image
+          src="/Hanapp-Logo-Registered.png"
+          alt="Hanapp Logo Mobile"
+          width={mobileLogoWidth}
+          height={mobileLogoHeight}
+          sizes={`(min-width: 640px) ${smMobileLogoWidth}px, ${mobileLogoWidth}px`}
+          className="object-contain block lg:hidden"
+          priority
+        />
+        <Image
+          src="/DialogLogo.png"
+          alt="Dialog header image Desktop"
+          width={desktopLogoWidth}
+          height={desktopLogoHeight}
+          className="object-contain hidden lg:block"
+          priority
+        />
+      </div>
 
       <div className="flex flex-wrap items-center justify-center gap-1 text-xl xs:text-2xl sm:text-3xl leading-snug px-4">
         <span className="text-[#F5C45E] font-semibold">Big</span>
