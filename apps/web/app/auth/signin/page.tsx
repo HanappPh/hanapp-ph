098b5235658c824525be72baba2f35a604bcf543
Aenubrix <anubrix326@gmail.com>
2025-09-25 15:04:53 +0800

    fix: login-ui

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -6,7 +6,7 @@ import { MainHeader } from '../../../components/login-header';
 
 export default function SigninPage() {
   return (
-    <>
+    <div className="h-screen w-screen overflow-hidden relative">
       <div className="hidden lg:block">
         <BackgroundImage imageSrc="/placeholder.jpg" />
       </div>
@@ -16,6 +16,6 @@ export default function SigninPage() {
       </div>
 
       <LoginDialog />
-    </>
+    </div>
   );
 }
