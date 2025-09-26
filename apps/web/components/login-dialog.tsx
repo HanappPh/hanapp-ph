42768551edf832b55be44ca3234a7fa7789beaf0
Aenubrix <anubrix326@gmail.com>
2025-09-26 15:26:03 +0800

    fix: shadcn components

--- apps/web/components/login-dialog.tsx
+++ apps/web/components/login-dialog.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Card } from '@hanapp-ph/commons';
+
 import { AuthButtons } from './login-auth-buttons';
 import { DialogHeader } from './login-dialog-header';
 
@@ -18,7 +20,7 @@ export function LoginDialog({
     <div
       className={`fixed inset-0 flex items-center justify-center lg:justify-end z-[100] ${className}`}
     >
-      <div className="w-full h-full rounded-none mx-0 bg-white shadow-2xl border-0 lg:max-w-[550px] xl:max-w-[650px] 2xl:max-w-[650px] lg:rounded-3xl lg:min-h-[min(850px,85vh)] lg:max-h-[min(900px,90vh)] lg:h-auto lg:mx-auto lg:mr-[10%]">
+      <Card className="w-full h-full rounded-none mx-0 bg-white shadow-2xl border-0 lg:max-w-[550px] xl:max-w-[650px] 2xl:max-w-[650px] lg:rounded-3xl lg:min-h-[min(850px,85vh)] lg:max-h-[min(900px,90vh)] lg:h-auto lg:mx-auto lg:mr-[10%]">
         <div className="flex flex-col h-full p-4 sm:p-12 justify-center items-center">
           <div className="flex flex-col space-y-8 lg:pt-0 xl:pt-8 2xl:pt-12">
             <div className="flex flex-col justify-center">
@@ -32,7 +34,7 @@ export function LoginDialog({
             </div>
           </div>
         </div>
-      </div>
+      </Card>
     </div>
   );
 }
