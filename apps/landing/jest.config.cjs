25b2785bc49c011d7e81dd8d22da49f3951bdc4c
KaoM3 <152694740+KaoM3@users.noreply.github.com>
2025-09-28 16:56:19 +0800

    fix: convert ESM TypeScript config files to CJS for app/landing

--- apps/landing/jest.config.cjs
+++ apps/landing/jest.config.cjs
@@ -1,11 +1,11 @@
-import type { Config } from 'jest';
-import nextJest from 'next/jest.js';
+
+const nextJest = require('next/jest.js');
 
 const createJestConfig = nextJest({
   dir: __dirname,
 });
 
-const config: Config = {
+const config = {
   displayName: '@hanapp-ph/landing',
   preset: '../../jest.preset.js',
   transform: {
@@ -16,4 +16,4 @@ const config: Config = {
   testEnvironment: 'jsdom',
 };
 
-export default createJestConfig(config);
+module.exports = createJestConfig(config);
