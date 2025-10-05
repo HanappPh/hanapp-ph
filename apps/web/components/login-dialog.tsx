5572745db1b132b98cd4dd3855bb69eb6aa0aec5
RugIsBroke <ry4njustns0@gmail.com>
2025-10-06 02:37:23 +0800

    fix: resolved log in page UI

--- apps/web/components/login-dialog.tsx
+++ apps/web/components/login-dialog.tsx
@@ -20,9 +20,9 @@ export function LoginDialog({
     <div
       className={`fixed inset-1 flex items-center justify-center lg:justify-end z-[100] ${className}`}
     >
-      <Card className="w-full h-full rounded-none mx-0 bg-white shadow-2xl border-0 lg:max-w-[550px] xl:max-w-[650px] 2xl:max-w-[650px] lg:rounded-3xl lg:min-h-[min(850px,85vh)] lg:max-h-[min(900px,90vh)] lg:h-auto lg:mx-auto lg:mr-[10%]">
-        <div className="flex flex-col flex-1 min-h-0 h-full p-4 sm:p-12 items-center justify-center">
-          <div className="flex flex-col flex-1 min-h-0 w-full max-w-xl items-center justify-center">
+      <Card className="w-full h-full rounded-none mx-0 bg-white shadow-2xl border-0 lg:max-w-[420px] xl:max-w-[480px] 2xl:max-w-[500px] lg:rounded-3xl lg:min-h-[min(750px,82vh)] lg:max-h-[min(800px,88vh)] lg:h-auto lg:mx-auto lg:mr-[10%]">
+        <div className="flex flex-col flex-1 min-h-0 h-full p-4 sm:p-8 items-center justify-center">
+          <div className="flex flex-col flex-1 min-h-0 w-full max-w-md items-center justify-center">
             <DialogHeader />
             <AuthButtons onSendOTP={onSendOTP} onGoogleLogin={onGoogleLogin} />
           </div>
