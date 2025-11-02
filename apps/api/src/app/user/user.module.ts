58f3b41d2011cbfc0822f1f8df7c8e4ac681ebde
Lance Reyes <lr.lancereyes@gmail.com>
2025-11-02 15:32:01 +0800

    feat: register authguard globally in the user module

--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 
+import { AuthGuard } from '../guards/auth.guard';
 import { SupabaseService } from '../services/supabase.service';
 
 import { UserController } from './user.controller';
@@ -7,7 +9,14 @@ import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, SupabaseService],
+  providers: [
+    UserService,
+    SupabaseService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [UserService],
 })
 export class UserModule {}
